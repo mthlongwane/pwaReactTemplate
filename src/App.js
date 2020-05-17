d9a04d014450edb12678a935d85261e2d2ce4d4b
Thando Hlongwane <hlnmzw001@myuct.ac.za>
2020-05-18 00:32:57 +0200

    React PwA Accessibility mods

--- src/App.js
+++ src/App.js
@@ -14,22 +14,32 @@ class App extends Component  {
           refreshCacheAndReload();
         }
         return(
-            <div className="App" data-test= "App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
+          <div className="App" data-test= "App">
+          <header className="App-header">
+          {//Must be atleast one header - PWA Accessibility Testing
+          }
+            <h1>React PWA</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            {//Must be atleast one skip link - PWA Accessibility Testing
+            }
+            <a
+              className="App-link skip-link"
+              href="#maincontent"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Skip to Main
+            </a>
+          </header>
+          {//Skip link skips to here - PWA Accessibility Testing
+          }
+          <main id="maincontent"> 
+              <p>main</p>
+          </main>
+        </div>
         );
       }}
     </CacheBuster>
